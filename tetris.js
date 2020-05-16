e55e0ffe69c445a964e71913f8c861ad683839f4
Darian Martinez <darian.mtz18@gmail.com>
2020-05-16 11:22:45 -0500

    currently working on the shapes

--- tetris.js
+++ tetris.js
@@ -5,6 +5,13 @@ document.addEventListener('DOMContentLoaded', () =>{
     const StartBtn = document.querySelector('#start-button')
     const width = 10
 
+    const lTetrominoes = [
+        [1, width+1, width*2+1, 2],
+        [1, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ] 
+
    /* const lTetrominoes = 'firstshape'
     const zTetrominoes = 'secondshape'
     const oTetrominoes = 'thirdshape'
